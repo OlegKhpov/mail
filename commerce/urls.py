c2732033b270856f4102af6241d4937d3f2da908
UlrichKh <UlrichKh@github.com>
2020-08-30 11:03:55 +0300

    Update 30/8 11:03

--- commerce/urls.py
+++ commerce/urls.py
@@ -19,15 +19,9 @@ from django.urls import include, path
 from django.conf.urls import url
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls"))
-]
-
-if settings.DEBUG:
-    urlpatterns += [
-        url(r'^/auctions/static/images/(?P<path>.*)$', serve, {
-            "document_root": settings.IMAGES_ROOT,
-        }),
-    ]
+] + static(settings.IMAGES_URL, document_root=settings.IMAGES_DIR)
